796480422ace04faa69bc165196293f7b578958d
Carlos Soria <soriac367@gmail.com>
2022-07-19 11:56:47 -0500

    Refactor

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -2,9 +2,7 @@
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-    if (!tree)
-        return (0);
-	if (!tree->right && !tree->left)
+    if ((!tree) || (!tree->right && !tree->left))
 		return (0);
 	if (tree->right)
 		return (binary_tree_height(tree->right) + 1);
