cfbc899aff21ca0c5b26eb6e483b11efd8c840bd
Carlos Soria <soriac367@gmail.com>
2022-07-19 11:12:38 -0500

    Binary tree tasks

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+    binary_tree_t *new_node;
+
+    if (!parent)
+        return NULL;
+    new_node = binary_tree_node(parent, value);
+    if (!new_node)
+        return NULL;
+    if (!parent->right)
+        parent->right = new_node;
+    else
+    {
+        new_node->right = parent->right;
+        parent->right = new_node;
+        new_node->right->parent = new_node;
+    }
+
+    return new_node;
+}
